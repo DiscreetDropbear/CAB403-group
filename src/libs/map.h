f2a2daf2627c3361dcbedbbc555a7d7f54d92fcf
Jake Martin <ajanimember@gmail.com>
2021-10-20 09:53:35 +1000

    changed the map implementation to be a hashmap based of of the one from the week 3 practical

--- src/libs/map.h
+++ src/libs/map.h
@@ -1,5 +1,6 @@
 #ifndef MAP_H
 #define MAP_H
+#include<stdbool.h>
 
 // the map assumes that all values stored within it are stored on the heap
 // you shouldn't point to a value that lives on the stack or any global memory
@@ -7,16 +8,35 @@
 // call free_map()
 
 // the initial size of the map, will grow as needed, we won't implement shrinking as we won't need too
-#define INIT_SIZE 8
+#define INIT_SIZE 8 
+// a percentage between 0 and 1 exclusive, specifies how many items to available spots there should
+// be before growing the map to keep any buckets from having too long of a linked list
+#define GROW_DENSITY 0.75
 
-// for now we will implement this map in a really niave way and if we need to optimise
-// it for speed reasons we will
+typedef struct pair pair_t;
+struct pair{
+    char* key;
+    void* value;
+};
 
-typedef struct map{
-    int size;
-    char** keys;         
-    void** values;
-} Map;
+typedef struct item item_t;
+struct item{
+    char* key;
+    void* value;
+    item_t *next;
+};
+
+typedef struct map Map; 
+struct map{
+    size_t size; // length of the buckets array
+    size_t items; // number of key:value pairs currently in the map
+    item_t ** buckets;
+};
+
+// returns the nth item that is in the map if 
+// from start to finish
+// this can be used to get a random value from the map
+pair_t get_nth_item(Map* map, size_t n);
 
 // sets up the map ready for use
 void init_map(Map* map, unsigned int initial_size);
@@ -30,8 +50,8 @@ void* insert(Map* map, char* key, void* value);
 
 // removes a key:value pair from the map returning the value given the
 // key exists in the map, other wise NULL is returned
-void* delete(Map* map, char* key);
+void* remove_key(Map* map, char* key);
 
 // returns true if the map has the given key
-int exists(Map* map, char* key);
+bool exists(Map* map, char* key);
 #endif
