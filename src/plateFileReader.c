19cf2482d90463fd5182ab14694ad3a4b0ca46cc
mclaulu <mclaulu@gmail.com>
2021-10-20 22:53:01 +1000

    re-wrote LPR file

--- src/plateFileReader.c
+++ src/plateFileReader.c
@@ -1,22 +1,22 @@
-FILE *fn;
-long lSize;
-char *buffer;
+int main()
+{
+    int i,j;
+    int value[100][7];
+    FILE *archivo;
+    archivo = fopen("plate.txt","r");
+    if (archivo == NULL)
+        exit(1);
+    i=0;
+    while (feof(archivo) == 0)
+    {
+        fscanf( archivo, "%c %c %c %c %c %c\n", &value[i][0],&value[i][1],&value[i][2],&value[i][3],&value[i][4],&value[i][5]);
+        printf("%c %c %c %c %c %c\n", value[i][0], value[i][1], value[i][2], value[i][3], value[i][4], value[i][5]);
+        i++;
+    }
 
-fn = fopen ( "plates.txt" , "rb" );
-if( !fn ) perror("plates.txt"),exit(1);
+    for(j = 0; j < i; j++)
+        printf("%c %c %c %c %c %c\n", value[j][0], value[j][1], value[j][2], value[j][3], value[j][4], value[j][5]);
 
-fseek( fn , 0L , SEEK_END);
-lSize = ftell( fn );
-rewind( fn );
-
-/* memory allocation */
-buffer = calloc( 1, lSize+1 );
-if( !buffer ) fclose(fn),fputs("memory alloc fails",stderr),exit(1);
-
-/* copy file into the buffer */
-if( 1!=fread( buffer , lSize, 1 , fn) )
-  fclose(fn),free(buffer),fputs("entire read fails",stderr),exit(1);
-
-
-fclose(fn);
-free(buffer);
+    fclose(archivo);
+    return 0;
+}
