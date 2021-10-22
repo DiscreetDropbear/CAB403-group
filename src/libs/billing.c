0227b35033194a92b5b3abde5fce2bec5e01a9af
Jake Martin <ajanimember@gmail.com>
2021-10-22 10:05:03 +1000

    changed the add rego to copy the key thats given so the caller can keep using it

--- src/libs/billing.c
+++ src/libs/billing.c
@@ -12,11 +12,15 @@ int init_billing(Billing* billing){
 
 void* insert_rego(Billing* billing, char * rego){
     assert(billing != NULL);
+    // copy the rego
+    char * regoc = malloc(7);
+    memcpy((void*)regoc, (void*)rego, 7);
+
     struct timespec* time = calloc(sizeof(struct timespec));
     struct timespec* res;
 	clock_gettime(CLOCK_MONOTONIC, time);
 
-    res = insert(&billing->map, rego, (void*)time);
+    res = insert(&billing->map, regoc, (void*)time);
 }
 
 // returns the timespec for a given rego removing it from the 
