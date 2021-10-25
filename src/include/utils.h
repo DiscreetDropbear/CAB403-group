aed841a7ddac309592f637b2063521767bb06351
Jake Martin <ajanimember@gmail.com>
2021-10-25 10:04:41 +1000

    added init_shared_mem function to utils.c because it is used for testing the manager

--- src/include/utils.h
+++ src/include/utils.h
@@ -2,6 +2,10 @@
 #define TIMING_H
 #include <time.h>
 
+// create the mutex's and condition variables and set them to PTHREAD_PROCESS_SHARED 
+// for each lpr, boom and sign in entrances, exits and levels
+void init_shared_mem(volatile void * shm);
+
 // get the difference between now and the given time in milliseconds
 // and stores it in size_t* milli
 // if clock_gettime() returns a non zero value that is returned and
